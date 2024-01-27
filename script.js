015b796b319afdce4d3f774f6ee743f2ec54420b
Kieran Wake <kieranwake1234@gmail.com>
2024-01-27 10:26:38 +0000

    implemented variable to convey the difference in matchsticks from house to house (5), then applied a let variable which we'll use to store our result value. proceeding to calculate the steps multiplied by the difference, adding 1 to each iteration

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const matchStick = (n) => {
     if (n === 0) {
         console.log("step 0 returns 0 matchsticks"); 
-    } else if (n > 0 || typeof n !== 'number') {
+    } else if (n < 0 || typeof n !== 'number') {
         console.log("input step will always be non-negative"); 
     } else {
-        let result = n; 
-        return result; 
+        const difference = 5;
+        let result = 0;
+        result = (n*5)+1; 
+        return result;
     }
-        
 }
 
-console.log(matchStick(8))
+console.log(matchStick(2))
