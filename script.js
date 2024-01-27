5847d9668d907919d9434edff9dfefba11b59f2c
Kieran Wake <kieranwake1234@gmail.com>
2024-01-27 10:17:31 +0000

    else if, n is greater than 0 or n's type isn't equal to a positive number

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 const matchStick = (n) => {
     if (n === 0) {
         console.log("step 0 returns 0 matchsticks"); 
-    } 
+    } else if (n > 0 || typeof n !== 'number') {
+        console.log("input step will always be non-negative"); 
+        let result = n; 
+        return result; 
+    }
 }
+
+console.log(matchStick())
